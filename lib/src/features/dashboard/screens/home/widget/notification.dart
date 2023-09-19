73bbdf14d3b7c9dd06b52652660ff18ad3b3c13c
DoanHoc <doanhoc172@gmail.com>
2023-09-19 21:54:26 +0700

    thong bao ui

--- lib/src/features/dashboard/screens/home/widget/notification.dart
+++ lib/src/features/dashboard/screens/home/widget/notification.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../constants/color.dart';
+
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
 
@@ -11,7 +13,57 @@ class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: const Text("Thông báo",
+              style: TextStyle(color: whiteColor, fontWeight: FontWeight.w700)),
+          backgroundColor: mainColor,
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: whiteColor // Số 10.0 ở đây là bán kính bo góc
+                ),
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return const Column(
+                  children: [
+                    ListTile(
+                      leading: Icon(
+                        Icons.mark_email_unread,
+                        size: 32,
+                        color: processColor,
+                      ),
+                      title: Row(
+                        children: [
+                          Text(
+                            "Sản phẩm sắp hết hạn",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w800, fontSize: 15),
+                          ),
+                          Text(
+                            " - 8 giờ trước",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ],
+                      ),
+                      subtitle: Text(
+                        'Bạn có sản phẩm "Sting" sắp hết hạn trong 7 ngày tới',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 18),
+                      child: Divider(),
+                    )
+                  ],
+                );
+              },
+            ),
+          ),
+        ));
   }
 }
