6bcdb10e5cfd52c29146a804c0778a0e53cd8c39
DoanHoc <doanhoc172@gmail.com>
2023-09-28 13:27:16 +0700

    xong phan nhap item

--- lib/src/features/dashboard/screens/add/nhaphang/nhaphang_screen.dart
+++ lib/src/features/dashboard/screens/add/nhaphang/nhaphang_screen.dart
@@ -3,6 +3,9 @@ import 'package:hobin_warehouse/src/constants/color.dart';
 import 'package:hobin_warehouse/src/constants/icon.dart';
 import 'package:hobin_warehouse/src/features/dashboard/screens/add/nhaphang/choose_goods.dart';
 import 'package:hobin_warehouse/src/features/dashboard/screens/add/nhaphang/choose_location.dart';
+import 'package:intl/intl.dart';
+
+import 'widget/chonsoluong_widget.dart';
 
 class NhapHangScreen extends StatefulWidget {
   const NhapHangScreen({super.key});
@@ -18,13 +21,68 @@ class _NhapHangScreenState extends State<NhapHangScreen> {
     {"icon": calendarIcon, "title": "Chọn ngày hết hạn"},
     {"icon": quantityIcon, "title": "Chọn số lượng"},
   ];
-
-  Map<String, String> thongTinItemNhap = {
+  List<Map<String, dynamic>> allThongTinItemNhap = [];
+  Map<String, dynamic> thongTinItemNhap = {
+    "macode": "",
     "hanghoa": "",
     "location": "",
     "exp": "",
-    "soluong": ""
+    "soluong": 0
   };
+  bool isButtonEnabled = false;
+  // Hàm kiểm tra xem tất cả các trường đã có dữ liệu hay chưa
+  void _checkFields() {
+    if (thongTinItemNhap["hanghoa"].isNotEmpty &&
+        thongTinItemNhap["location"].isNotEmpty &&
+        thongTinItemNhap["exp"].isNotEmpty &&
+        thongTinItemNhap["soluong"] > 0) {
+      setState(() {
+        isButtonEnabled = true;
+      });
+    } else {
+      setState(() {
+        isButtonEnabled = false;
+      });
+    }
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2101),
+    );
+
+    if (picked != null) {
+      setState(() {
+        thongTinItemNhap["exp"] = DateFormat('dd/MM/yyyy').format(picked);
+        _checkFields();
+      });
+    }
+  }
+
+  Future<void> _chonSoluong(BuildContext context) async {
+    String soluong = await showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      builder: (BuildContext context) {
+        return const ChonSoLuongWidget();
+      },
+    );
+    if (soluong.isNotEmpty) {
+      setState(() {
+        thongTinItemNhap["soluong"] = int.tryParse(soluong);
+        _checkFields();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +113,8 @@ class _NhapHangScreenState extends State<NhapHangScreen> {
                         if (value != null) {
                           setState(() {
                             thongTinItemNhap["hanghoa"] = value["tensanpham"];
+                            thongTinItemNhap["macode"] = value["macode"];
+                            _checkFields();
                           });
                         }
                       });
@@ -68,10 +128,16 @@ class _NhapHangScreenState extends State<NhapHangScreen> {
                         if (value != null) {
                           setState(() {
                             thongTinItemNhap["location"] = value["id"];
-                            print(thongTinItemNhap);
+                            _checkFields();
                           });
                         }
                       });
+                    }
+                    if (index == 2) {
+                      _selectDate(context);
+                    }
+                    if (index == 3) {
+                      _chonSoluong(context);
                     }
                   },
                   child: Container(
@@ -91,29 +157,29 @@ class _NhapHangScreenState extends State<NhapHangScreen> {
                           const SizedBox(width: 7),
                           Flexible(
                             child: index == 0 &&
-                                    thongTinItemNhap["hanghoa"]!.isNotEmpty
+                                    thongTinItemNhap["hanghoa"].isNotEmpty
                                 ? Text(
-                                    thongTinItemNhap["hanghoa"]!,
+                                    thongTinItemNhap["hanghoa"],
                                     style: const TextStyle(fontSize: 17),
                                   )
                                 : index == 1 &&
-                                        thongTinItemNhap["location"]!.isNotEmpty
+                                        thongTinItemNhap["location"].isNotEmpty
                                     ? Text(
-                                        thongTinItemNhap["location"]!,
+                                        thongTinItemNhap["location"],
                                         style: const TextStyle(fontSize: 17),
                                       )
                                     : index == 2 &&
-                                            thongTinItemNhap["exp"]!.isNotEmpty
+                                            thongTinItemNhap["exp"].isNotEmpty
                                         ? Text(
-                                            thongTinItemNhap["exp"]!,
+                                            thongTinItemNhap["exp"],
                                             style:
                                                 const TextStyle(fontSize: 17),
                                           )
                                         : index == 3 &&
-                                                thongTinItemNhap["soluong"]!
-                                                    .isNotEmpty
+                                                thongTinItemNhap["soluong"] != 0
                                             ? Text(
-                                                thongTinItemNhap["soluong"]!,
+                                                thongTinItemNhap["soluong"]
+                                                    .toString(),
                                                 style: const TextStyle(
                                                     fontSize: 17),
                                               )
@@ -132,6 +198,52 @@ class _NhapHangScreenState extends State<NhapHangScreen> {
             },
           )
         ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: 70,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            LayoutBuilder(builder: (context, constraints) {
+              return SizedBox(
+                width: MediaQuery.of(context).size.width - 30,
+                height: 45,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    backgroundColor: blueColor,
+                    side: BorderSide(
+                        color: isButtonEnabled ? blueColor : Colors.black12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          10), // giá trị này xác định bán kính bo tròn
+                    ),
+                  ),
+                  onPressed: isButtonEnabled
+                      ? () {
+                          setState(() {
+                            allThongTinItemNhap.add(thongTinItemNhap);
+                            thongTinItemNhap = {
+                              "macode": "",
+                              "hanghoa": "",
+                              "location": "",
+                              "exp": "",
+                              "soluong": 0
+                            };
+                            _checkFields();
+                            print(allThongTinItemNhap);
+                          });
+                        }
+                      : null,
+                  child: const Text(
+                    'Thêm',
+                    style: TextStyle(fontSize: 19),
+                  ),
+                ),
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
