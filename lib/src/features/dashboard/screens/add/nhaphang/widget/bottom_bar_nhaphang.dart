c3df7db85b7c1bb081bdc613515db4f2c4bb2f49
DoanHoc <112065755+DoanHoc@users.noreply.github.com>
2023-09-29 00:23:29 +0700

    delete print (#3)

--- lib/src/features/dashboard/screens/add/nhaphang/widget/bottom_bar_nhaphang.dart
+++ lib/src/features/dashboard/screens/add/nhaphang/widget/bottom_bar_nhaphang.dart
@@ -35,7 +35,6 @@ class _BottomBarNhapHangState extends State<BottomBarNhapHang> {
 
   @override
   Widget build(BuildContext context) {
-    print("1");
     return BottomAppBar(
       height: 70,
       child: Row(
