5e8ba661946e5d21c7cf63bc2acc91638f100d3c
DoanHoc <doanhoc172@gmail.com>
2023-10-14 20:01:57 +0700

    controller text form field

--- lib/src/features/dashboard/controllers/statistics/no_controlller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-class NoController extends GetxController {
-  static NoController get instance => Get.find();
-
-  TextEditingController traNoController = TextEditingController();
-}
